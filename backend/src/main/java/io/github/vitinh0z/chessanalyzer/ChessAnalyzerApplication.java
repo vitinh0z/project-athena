aedafab9e219ea698abd799b9f860574244b90c2
Victor Gabriel <victor10.vg23@gmail.com>
2026-03-22 15:15:32 -0300

    fix: problema de build

--- backend/src/main/java/io/github/vitinh0z/chessanalyzer/ChessAnalyzerApplication.java
+++ backend/src/main/java/io/github/vitinh0z/chessanalyzer/ChessAnalyzerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChessAnalyzerApplication {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(ChessAnalyzerApplication.class, args);
     }
 }
